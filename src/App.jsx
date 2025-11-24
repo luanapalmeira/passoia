31be7c60f7e0612ae8453e89e9025f16512f9f7d
Luana Palmeira <luana.lp29@outlook.com>
2025-11-24 18:06:31 -0300

    Criação e estilização do componente banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 import Lancamentos from "./components/Lancamentos";
 import "./global.scss";
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Banner />
       <Lancamentos />
     </>
   );
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,8 @@
+import "./banner.scss";
+// import banner from "../../assets/Banner/Banner.png";
+
+function Banner() {
+  return <section></section>;
+}
+
+export default Banner;
