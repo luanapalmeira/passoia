c612d77914535d23f352db09ca81d7f27ea9de83
Luana Palmeira <luana.lp29@outlook.com>
2025-11-27 03:50:16 -0300

    Estilização do componente Lançamentos

--- src/components/Lancamentos/index.jsx
+++ src/components/Lancamentos/index.jsx
@@ -13,7 +13,7 @@ import kitGloss from "../../assets/Lancamentos/kit-gloss.png";
 import kitBatomLiquido from "../../assets/Lancamentos/kit-batom-liquido.png";
 import kitBatomBala from "../../assets/Lancamentos/kit-batom-em-bala.png";
 import setaEsquerda from "../../assets/Lancamentos/seta-esquerda.png";
-import setaDireita from "../../assets/Lancamentos/seta-direita.png"
+import setaDireita from "../../assets/Lancamentos/seta-direita.png";
 
 function Lancamentos() {
   // Funcionalidade do React que armazena estados.
@@ -24,61 +24,64 @@ function Lancamentos() {
   const carouselRef = useRef(null);
 
   const scrollLeft = () => {
-  carouselRef.current.scrollBy({
-    left: -260,
-    behavior: "smooth",
-  });
-};
+    carouselRef.current.scrollBy({
+      left: -260,
+      behavior: "smooth",
+    });
+  };
 
-const scrollRight = () => {
-  carouselRef.current.scrollBy({
-    left: 260,
-    behavior: "smooth",
-  });
-};
+  const scrollRight = () => {
+    carouselRef.current.scrollBy({
+      left: 260,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <section className="lancamentos">
       <h2>Aproveite os lançamentos</h2>
-      <div className="imagens-kits">
-        <img src={kitGloss} alt="Foto de 2 glosses" />
-        <img src={kitBatomLiquido} alt="Foto de 3 batons líquidos" />
-        <img src={kitBatomBala} alt="Foto de 3 batons em bala" />
-      </div>
-
-      <img src={cor} alt="Modelo usando os batons de demonstração" />
-
-      <article className="box-testagem">
-        <img src={estrelas} alt="Estrelas de avaliação" />
-        <h4>Matte Premium</h4>
-        <p className="subtitulo">Cores disponíveis</p>
-
-        <div className="carrossel-container">
-          <button className="nav-btn" onClick={scrollLeft}> <img src={setaEsquerda} alt="ícone de seta para a esquerda" /> </button>
-
-          <div className="carrossel" ref={carouselRef}>
-            <button id="azul" onClick={() => setCor(azul)}>Azul</button>
-            <button id="vermelho" onClick={() => setCor(vermelho)}>vermelho</button>
-            <button id="marrom" onClick={() => setCor(marrom)}>Marrom</button>
-            <button id="base" onClick={() => setCor(base)}>base</button>
-            <button id="rosa" onClick={() => setCor(rosa)}>rosa</button>
-            <button id="vermelhoR" onClick={() => setCor(vermelhoRosado)}>vermelhor</button>
-            <button id="lilas" onClick={() => setCor(lilas)}>lilás</button>
-            <button id="marromE" onClick={() => setCor(marromEscuro)}>marromE</button>
-          </div>
-
-          <button className="nav-btn" onClick={scrollRight}> <img src={setaDireita} alt="ícone de seta para a direita" /> </button>
+      <div className="alinhamento">
+        <div className="imagens-kits">
+          <img src={kitGloss} alt="Foto de 2 glosses" />
+          <img src={kitBatomLiquido} alt="Foto de 3 batons líquidos" />
+          <img src={kitBatomBala} alt="Foto de 3 batons em bala" />
         </div>
 
-        <p className="subtitulo-2">Descrição</p>
-        <p className="descricao">
-          O Batom Matte possui uma fórmula inovadora desenvolvida para entregar
-          o máximo de cor na primeira aplicação com um deslize suave e macio.
-          Tem acabamento matte aveludado e manteiga de manga que ajuda a
-          hidratar e a proteger os lábios contra ressecamento. Ajuda na
-          hidratação dos lábios, textura fina e macia que não pesa nos lábios.
-        </p>
-      </article>
+        <img src={cor} alt="Modelo usando os batons de demonstração" id="modelo" />
+
+        <article className="box-testagem">
+          <img src={estrelas} alt="Estrelas de avaliação" id="estrelas" />
+          <h4>Matte Premium</h4>
+          <p className="subtitulo">Cores disponíveis</p>
+
+          <div className="carrossel-container">
+            <button className="nav-btn" onClick={scrollLeft}>{" "} <img src={setaEsquerda} alt="ícone de seta para a esquerda" id="seta-esquerda" /> {" "}</button>
+
+            <div className="carrossel" ref={carouselRef}>
+              <button id="azul" onClick={() => setCor(azul)}></button>
+              <button id="vermelho" onClick={() => setCor(vermelho)}></button>
+              <button id="marrom" onClick={() => setCor(marrom)}></button>
+              <button id="base" onClick={() => setCor(base)}></button>
+              <button id="rosa" onClick={() => setCor(rosa)}></button>
+              <button id="vermelhoR" onClick={() => setCor(vermelhoRosado)}></button>
+              <button id="lilas" onClick={() => setCor(lilas)}></button>
+              <button id="marromE" onClick={() => setCor(marromEscuro)}></button>
+            </div>
+
+            <button className="nav-btn" onClick={scrollRight}>{" "} <img src={setaDireita} alt="ícone de seta para a direita" id="seta-direita" /> {" "}</button>
+          </div>
+
+          <p className="subtitulo-2">Descrição</p>
+          <p className="descricao">
+            O Batom Matte possui uma fórmula inovadora desenvolvida para
+            entregar o máximo de cor na primeira aplicação com um deslize suave
+            e macio. Tem acabamento matte aveludado e manteiga de manga que
+            ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda
+            na hidratação dos lábios, textura fina e macia que não pesa nos
+            lábios.
+          </p>
+        </article>
+      </div>
     </section>
   );
 }
